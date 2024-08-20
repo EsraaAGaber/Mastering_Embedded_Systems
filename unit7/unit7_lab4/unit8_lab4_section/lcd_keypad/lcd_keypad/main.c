6670619fecaa0b4b55b5295dc14ef86359260a1b
EsraaAGaber <55esrasgaber@gmail.com>
2024-08-21 02:13:41 +0300

    add

---
+++ unit7/unit7_lab4/unit8_lab4_section/lcd_keypad/lcd_keypad/main.c
@@ -0,0 +1,22 @@
+
+#include "lcd.h"
+
+
+
+int main(void)
+{LCD_INIT();
+	/* Replace with your application code */
+	while (1)
+	{
+		
+		LCD_WRITE_STRING("Learn_In_Depth..");
+		_delay_ms(2000);
+		LCD_clear_screen();
+		LCD_WRITE_STRING("Esraa Gaber");
+		_delay_ms(2000);
+		LCD_clear_screen();
+
+	
+	}
+	
+}
